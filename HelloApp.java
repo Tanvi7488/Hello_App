bbf1faa5de560037a0d8af15f3cb45845ec2045b
Tanvi <atanvi928@gmail.com>
2026-03-11 22:48:46 +0530

    UC2 Added

--- HelloApp.java
+++ HelloApp.java
@@ -1,8 +1,14 @@
-class HelloApp{
-	public static void main(String[] args){
-		//variable and datatypes
-	int num=100;
-	System.out.println(num);
-	System.out.println("Hello All...");
-	}
+public class HelloApp {
+    public static void main(String[] args) {
+
+        String name;
+
+        if (args.length > 0) {
+            name = args[0];
+        } else {
+            name = "World";
+        }
+
+        System.out.println("Hello, " + name + "!");
+    }
 }
