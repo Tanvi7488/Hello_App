4b8e69e7f0b08c989d4816ce62bd6c7e9002a323
Tanvi <atanvi928@gmail.com>
2026-03-25 14:13:38 +0530

    UC7 Added

--- HelloApp.java
+++ HelloApp.java
@@ -1,29 +1,19 @@
 /**
- * UC6: HelloApp
- * Accepts multiple names as command-line arguments and prints them
- * in a comma-separated greeting format.
+ * UC7: HelloApp
+ * Display "Hello" with multiple command-line arguments using String.join()
  */
 
 public class HelloApp {
 
     public static void main(String[] args) {
 
-        // If no names are given
-        if (args.length == 0) {
-            System.out.println("Hello, World!");
-            return;
+        // Default name if no arguments are provided
+        String names = "World";
+
+        // If user provides names, join them with comma and space
+        if (args.length > 0) {
+            names = String.join(", ", args);
         }
-
-        // Use StringBuilder to combine names
-        StringBuilder nameBuilder = new StringBuilder();
-
-        // Enhanced for loop to iterate through arguments
-        for (String name : args) {
-            nameBuilder.append(name).append(", ");
-        }
-
-        // Remove the last comma and space
-        String names = nameBuilder.substring(0, nameBuilder.length() - 2);
 
         // Print greeting
         System.out.println("Hello, " + names + "!");
